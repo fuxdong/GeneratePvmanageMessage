098e0a509822754273440e798560569953e4b259
Unknown <761302501@qq.com>
2017-08-26 01:14:11 +0800

    生成11个逆变器日、月、年、总发电量，环境数据报文

---
+++ GernatePvmanageNews/src/main/java/com/visenergy/generateRandomData/Test.java
@@ -0,0 +1,31 @@
+package com.visenergy.generateRandomData;
+
+import com.flying.logging.Log;
+import com.flying.logging.LogFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Fuxudong on 2017/8/25.
+ */
+public class Test {
+    public static void main(String[] args){
+
+        Log log = LogFactory.getLog(Test.class);
+        int a = 0;
+
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                log.info("报文1：" + GenerateMinuateData.getMinuatesData1());
+                log.info("报文2：" + GenerateMinuateData.getMinuatesData2());
+            }
+        };
+
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleWithFixedDelay(runnable, 0, 30, TimeUnit.SECONDS);
+    }
+}
